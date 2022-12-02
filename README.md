9344722b602de88ff29938b52896b28e53939bd7
Abhinaba Basu <abhinaba@live.com>
2022-12-02 10:52:58 -0800

    script to deploy all together

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ How it works
 
 ```mermaid
 graph TD;
-    Web[Single Page App] --> RevProxy[nginx container on :80];
+    Web[Single Page App] --> |REST| RevProxy[nginx container on :80];
     RevProxy --> |/| default[index.html];
     RevProxy --> |/elemental| elemental[elemental app container :8080];
     RevProxy --> |/wordament| wordament[wordament app container :8090];
@@ -22,7 +22,14 @@ Build
 
 Deploy
 ------
+Just this container can be run with
 ``` bash
 sudo bash
 curl -fsSL "https://raw.githubusercontent.com/abhinababasu/infra/main/deploy.sh?token=GHSAT0AAAAAAB26S7FFMCSRHQ23PFOQPDTGY4HBHLA" | bash
+```
+
+All containers can be depoloyed with
+``` bash
+sudo bash
+curl -fsSL "https://raw.githubusercontent.com/abhinababasu/infra/main/deployall.sh?token=GHSAT0AAAAAAB26S7FFMCSRHQ23PFOQPDTGY4HBHLA" | bash
 ```
---
+++ deployall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# curl -fsSL get.docker.com -o get-docker.sh
+# sh get-docker.sh
+
+echo This deploys all the apps and rev-proxy
+
+echo Stopping Wordament
+docker ps --filter="ancestor=bonggeek/wordament" -q | xargs docker stop
+
+echo Stopping Elemental
+docker ps --filter="ancestor=bonggeek/elementservice" -q | xargs docker stop
+
+echo Stopping nginx-rev-proxy
+docker ps --filter="ancestor=bonggeek/nginx-rev-proxy" -q | xargs docker stop
+
+echo pulling all images
+docker pull bonggeek/nginx-rev-proxy
+docker pull bonggeek/elementservice
+docker pull bonggeek/wordament
+
+echo start nginx
+docker run -d --restart="always" -p 80:80 bonggeek/nginx-rev-proxy
+
+echo start wordament on 8090
+docker run -d --restart="always" -p 8090:8090 bonggeek/wordament
+
+echo start elemental on 8080
+docker run -d --restart="always" -p 8080:8080 bonggeek/elementservice
